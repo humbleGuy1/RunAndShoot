6bfc760a9e02d1a32492a54eaf20d8f58c10b55d
humbleGuy1 <vrudnevskiy@list.ru>
2022-05-19 22:21:12 +0300

    Commit21

--- Assets/Scripts/Player/MovementLimiter.cs
+++ Assets/Scripts/Player/MovementLimiter.cs
@@ -1,22 +1,13 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Player))]
-
 public class MovementLimiter : MonoBehaviour
 {
 
     [SerializeField] private float _sideBorder;
 
-    private Player _player;
-
-    private void Start()
-    {
-        _player = GetComponent<Player>();
-    }
-
     private void Update()
     {
-        _player.transform.position = new Vector3(Mathf.Clamp(_player.transform.position.x, -_sideBorder, _sideBorder),
-            transform.position.y, _player.transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -_sideBorder, _sideBorder),
+            transform.position.y, transform.position.z);
     }
 }
